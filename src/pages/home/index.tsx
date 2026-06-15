4f040e5d4f1182d61cd3151bff0e6904a42e89d6
Raphael-Aljona <raphael.aljona@gmail.com>
2026-06-15 10:54:18 -0300

    feat: Seção banner da tela home feita

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,9 +4,9 @@ const Home = () => {
     return (
         <>
             <Header></Header>
-                <main className="vh-100" style={{ backgroundColor: "var(--preto-fundo)" }}>
+                <main className="vh-100 d-flex align-items-center" style={{ backgroundColor: "var(--preto-fundo)" }}>
                 <section className="h-75 d-flex align-items-center flex-column justify-content-center h-75 container" >
-                    <img className="h-100 col-12 object-fit-lg-cover" src="/imgs/ImageHome.png" alt=""/>
+                    <img className="h-100 col-12 object-fit-lg-cover rounded-4" src="/imgs/ImageHome.png" alt=""/>
                     <div className="col-10 position-relative d-flex justify-content-between p-4 z-3 glass" style={{bottom: "50px"}}>
                         <div className="col-6 d-flex flex-column">
                             <label className="text-white" htmlFor="">Selecione o Serviço</label>
