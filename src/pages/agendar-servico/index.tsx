40f2a727c416da1caa8a7f84f567ca89c8aa5e5a
S4les-exe <arthurnogueira54477@gmail.com>
2026-06-17 11:01:35 -0300

    tsx pronto só falta estilizar

--- src/pages/agendar-servico/index.tsx
+++ src/pages/agendar-servico/index.tsx
@@ -1,6 +1,7 @@
 import styles from "@/pages/agendar-servico/agendar-servico.module.css"
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import ButtonGold from "../components/ButtonGold";
 
 const AgendarServico = () => {
     return(
@@ -10,7 +11,39 @@ const AgendarServico = () => {
                     <div className={`${styles.container_agendar} layout_guide`}>
                         <h1>AGENDAR SERVIÇO</h1>
                         <form action="post" className={styles.formulario_servico}>
-                            
+                            <div className={styles.div_profissionais_servico}>
+                                <div>
+                                    <label>Selecione os serviços</label>
+                                    <select name="" id="">
+                                        <option>Vitu</option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <label>Selecione os profissionais</label>
+                                    <select name="" id="">
+                                        <option value="">eduardo</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className={styles.div_data}>
+                                <label>Selecione a data</label>
+                                <input type="text" />
+                            </div>
+                            <div className={styles.div_hora_inicio_fim}>
+                                <div>
+                                    <label>Selecione a hora inicio</label>
+                                    <input type="text" />
+                                </div>
+                                <div>
+                                    <label>Selecione a hora fim</label>
+                                </div>
+                            </div>
+                            <div className={styles.div_observacoes}>
+                                <label>Observações</label>
+                                <input type="text" />
+                            </div>
+
+                            <ButtonGold value="Agendar"/>
                         </form>
                     </div>
                 </section>
