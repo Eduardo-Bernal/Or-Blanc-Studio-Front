05750cd955d9f9066fa1707022b19f1c211e1801
Eduardo-Bernal <cbedudu@gmail.com>
2026-06-21 15:34:59 -0300

    feat: agendar servico

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 import type { AppProps } from "next/app";
-import { AuthProvider } from "@/contexts/AuthContext";
+import {AuthProvider} from "@/pages/api/AuthContext";
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
